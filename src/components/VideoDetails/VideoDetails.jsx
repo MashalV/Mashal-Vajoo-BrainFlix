4bc47fe10db5c6d62abca45e4689961b49b4b49f
@MashalV <mashalvajoo@gmail.com>
2024-08-17 19:26:50 -0400

    comments styling and input

--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -54,16 +54,42 @@ function VideoDetails({ video }) {
           </div>
         </div>
       </section>
-      <section className ="mainVid__description">
+      <section className="mainVid__description">
         <p>{description}</p>
       </section>
-      <section>
+
+    <section className="comments">
+        <div>amount of comments???</div>
+        <card className="card">
+            <img className="card__image"src="/src/assets/images/Mohan-muruge.jpg" alt ="picture of mohan"/>
+				<form className="card__wrapper">
+                        <label className="card__wrapper--comment">
+                            JOIN THE CONVERSATION
+                            <input className="card__wrapper--comment--in" type="text" name="comment" placeholder="Add a new comment" required/>
+                        </label>
+                        <button className="card__wrapper--btn btn">
+                            <img className="btn__img" src="/src/assets/images/icons/add_comment.svg" alt="add comment icon" />
+                            <div className = "btn__text">COMMENT</div>
+                        </button>
+                </form>
+        </card>
+    </section>
+
+
+        <section>
         {comments.map((comment) => (
           <div key={comment.id} className="comment">
-            <h4>{comment.name}</h4>
-            <p>{comment.comment}</p>
-            <span>{new Date(comment.timestamp).toLocaleDateString()}</span>
-          </div>
+            <div className= "comment__avatar"></div>
+            <div className = "comment__wrapper">
+                <div className = "comment__wrapper--text">
+                    <div class= "comment__wrapper--text--box box">
+                        <h3 className = "box__name">{comment.name}</h3>
+                        <span className = "box__date">{new Date(comment.timestamp).toLocaleDateString()}</span>
+                    </div>
+                </div>
+                    <p className="comment__wrapper--details">{comment.comment}</p>
+            </div>
+            </div>
         ))}
       </section>
     </>
